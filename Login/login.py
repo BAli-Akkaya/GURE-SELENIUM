abf6905694c5ce90f9a5d3efae26966f90665870
BAli-Akkaya <ali.akkaya@niltekyazilim.com.tr>
2025-07-03 16:22:15 +0300

    düzenleme

--- Login/login.py
+++ Login/login.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager  # EKLENDİ
 import time
@@ -63,3 +62,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
